5ea501a411fc89fcd7a768a55017278d55e6cbf8
lynch1972 <pmcramos@gmail.com>
2022-11-25 15:52:05 +0100

    Commit

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from points import Point
 
 
 def main():
-    p1 = Point(a=1, b=2)
-    print(f"x= {p1.x}")
-    p1.x = 10
-    print(f"x= {p1.x}")
-    del p1.x
-    print(f"x= {p1.x}")
-    p1.x = p1.start_x_value
-    print(f"x= {p1.x}")
+    """Main Function"""
+    point_1 = Point(val_a=1, val_b=2)
+    print(f"x_val= {point_1.x_val}")
+    point_1.x_val = 10
+    print(f"x_val= {point_1.x_val}")
+    del point_1.x_val
+    print(f"x_val= {point_1.x_val}")
+    point_1.x_val = point_1.start_x_val_value
+    print(f"x_val= {point_1.x_val}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- points.py
+++ points.py
@@ -6,29 +6,33 @@ from dataclasses import dataclass, field
 @dataclass(order=True, slots=True)
 class Point:
     """Point Class"""
-    a: float = 0
-    b: float = 0
-    _x: float | None = field(init=False, repr=False, default=None)
+
+    val_a: float = 0
+    val_b: float = 0
+    _x_val: float | None = field(init=False, repr=False, default=None)
 
     def __post_init__(self):
         """Post Initializer"""
 
     @property
-    def start_x_value(self) -> float:
-        return self.a + self.b
+    def start_x_val_value(self) -> float:
+        """Starting Value to be assigned if desired"""
+        return self.val_a + self.val_b
 
     @property
-    def x(self) -> float:
-        print(f"Get x that previously was {self._x}")
-        return self._x
+    def x_val(self) -> float:
+        """x_val Getter Method"""
+        print(f"Get x_val that previously was {self._x_val}")
+        return self._x_val
 
-    @x.setter
-    def x(self, new_x: float) -> None:
-        print(f"Set x from {self._x} to {new_x}")
-        self._x = new_x
+    @x_val.setter
+    def x_val(self, new_x: float) -> None:
+        """x_val Setter Method"""
+        print(f"Set x_val from {self._x_val} to {new_x}")
+        self._x_val = new_x
 
-    @x.deleter
-    def x(self) -> None:
-        print("Delete X")
-        self._x = None
-
+    @x_val.deleter
+    def x_val(self) -> None:
+        """x_val Deleter Method"""
+        print("Delete x_val")
+        self._x_val = None
